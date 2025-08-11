aafb4d29eb5003241cf5a40ca0d86c43e2f9a781
Moshe Blumberg <mosheb@hotmail.co.uk>
2025-08-11 22:22:42 +0300

    Initial Commit

---
+++ tideTimes/tideTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tideTimesApp.swift
+//  tideTimes
+//
+//  Created by Moshe Blumberg on 11/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct tideTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tideTimesTests/tideTimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  tideTimesTests.swift
+//  tideTimesTests
+//
+//  Created by Moshe Blumberg on 11/08/2025.
+//
+
+import Testing
+@testable import tideTimes
+
+struct tideTimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
